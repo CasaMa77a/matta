0f1d4ca20992575272f1fae8172141fa52dcc5a0
CasaMa77a <117276660+CasaMa77a@users.noreply.github.com>
2025-11-11 09:44:44 +0100

    Add Cloudinary integration for media storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,4 +5,19 @@ export default ({ env }) => ({
       displayField: 'nome'
     }
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('Matta'),
+        api_key: env('279785463378244'),
+        api_secret: env('mDWwqUzcZ3EuVZPJQtfitzjMhSY'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
