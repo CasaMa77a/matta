c813fcd789e400cf94e9ec51736d4bd25c145d88
CasaMa77a <117276660+CasaMa77a@users.noreply.github.com>
2026-02-18 18:02:49 +0100

    Add fair content-type

---
+++ src/api/fair/controllers/fair.ts
@@ -0,0 +1,7 @@
+/**
+ * fair controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::fair.fair');
---
+++ src/api/fair/routes/fair.ts
@@ -0,0 +1,7 @@
+/**
+ * fair router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::fair.fair');
